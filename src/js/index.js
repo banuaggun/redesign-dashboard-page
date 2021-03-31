5b72f0fcd578aca5eaef3701e2e8e0034a9ad76f
Banu AĞGÜN <37499235+banuaggun@users.noreply.github.com>
2021-03-31 15:36:28 +0300

    tab menu edits

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,14 @@
-function infoTab(evt, cityName) {
+var list = document.getElementById("tab");
+var links = list.getElementsByClassName("tablinks");
+for (var i = 0; i < links.length; i++) {
+  links[i].addEventListener("click", function () {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
+/*
+function infoTab(evt, linkName) {
   var i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
@@ -8,8 +18,10 @@ function infoTab(evt, cityName) {
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
-  document.getElementById(cityName).style.display = "block";
+  document.getElementById(linkName).style.display = "block";
   evt.currentTarget.className += " active";
 }
 // Get the element with id="defaultOpen" and click on it
 document.getElementById("defaultOpen").click();
+
+*/
