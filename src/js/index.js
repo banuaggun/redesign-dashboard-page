ac332bb62984e5102b3cd7a2a519817677b3e52e
Banu AĞGÜN <37499235+banuaggun@users.noreply.github.com>
2021-04-03 11:23:46 +0300

    added jquery codes

--- src/js/index.js
+++ src/js/index.js
@@ -1,27 +1,10 @@
-var list = document.getElementById("tab");
-var links = list.getElementsByClassName("tablinks");
-for (var i = 0; i < links.length; i++) {
-  links[i].addEventListener("click", function () {
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
+$(document).ready(function () {
+  $(".tab-menu a").click(function (event) {
+    event.preventDefault();
+    $(this).parent().addClass("current");
+    $(this).parent().siblings().removeClass("current");
+    var tab = $(this).attr("href");
+    $(".tab-content").not(tab).css("display", "none");
+    $(tab).fadeIn();
   });
-}
-/*
-function infoTab(evt, linkName) {
-  var i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-  document.getElementById(linkName).style.display = "block";
-  evt.currentTarget.className += " active";
-}
-// Get the element with id="defaultOpen" and click on it
-document.getElementById("defaultOpen").click();
-
-*/
+});
